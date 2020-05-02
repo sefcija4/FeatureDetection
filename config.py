e4ca50db8bc57b24e03b2e1979e95bccba8843e1
Jan Šefčík <Sefcik.Jan@email.cz>
2020-05-02 17:00:46 +0200

    add config handler class

---
+++ config.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+
+data = dict()
+data['metadata'] = []
+data['input_img'] = []
+data['gps'] = []
+data['matching'] = []
+
+PATH = 'config.json'
+
+# METADATA
+data['metadata'].append({
+    'metadata_path': 'data.txt'
+})
+
+# INPUT IMAGE
+data['input_img'].append({
+    'path': 'data\\_p\\test.jpg'
+})
+
+# GPS
+data['gps'].append({
+    'radius': '0.003'
+})
+
+# MATCHING
+data['matching'].append({
+    'pixel_distance': '100',
+    'min_number_of_matches': '10',
+    'flann_index': '1',
+    'flann_trees': '5',
+    'flann_checks': '100'
+})
+
+
+with open(PATH, 'w+') as file:
+    json.dump(data, file)
---
+++ config_handler.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+
+
+class Config(object):
+
+    def __init__(self, path):
+
+        self.path = path
+        self.data = None
+
+        self.load()
+
+    def load(self):
+        """
+        Load json from self.path and set self.data
+        """
+        with open(self.path, 'r') as file:
+            self.data = json.load(file)
+            file.close()
+
+    def get_metadata(self):
+        """
+        Get metadata from config file
+        :return: (string) metadata
+        """
+        for p in self.data['metadata']:
+            metadata = p['metadata_path']
+
+        # print(metadata)
+        return metadata
+
+    def get_input_image(self):
+        """
+        Get path to input image from config file
+        :return: (string) path to input image
+        """
+        for p in self.data['input_img']:
+            path = p['path']
+
+        # print(path)
+        return path
+
+    def get_gps_radius(self):
+        """
+        Get gps radius from config file
+        :return: (float) radius
+        """
+        for p in self.data['gps']:
+            radius = float(p['radius'])
+
+        # print(radius)
+        return radius
+
+    def get_matching_setup(self):
+        """
+        Get matching setup from config file
+        :return: (dict) matching setup data
+        -
+        data['pixel_distance'] - minimal distance between keypoints (in pixels)
+        data['min_number_of_matches'] - minimal number of matches to successful match (less then that won't be
+                                        considered as possible match)
+        data['flann_index'] - index for flann matcher. For choosing structure
+        data['flann_trees'] - number of trees
+        data['flann_checks'] - maximal number of return in recursion
+        """
+        data = dict()
+
+        for p in self.data['matching']:
+            data['pixel_distance'] = int(p['pixel_distance'])
+            data['min_number_of_matches'] = int(p['min_number_of_matches'])
+            data['flann_index'] = int(p['flann_index'])
+            data['flann_trees'] = int(p['flann_trees'])
+            data['flann_checks'] = int(p['flann_checks'])
+
+        # print(data['pixel_distance'], data['min_number_of_matches'], data['flann_index'], data['flann_trees'], data['flann_checks'])
+
+        return data
+
+
+config = Config('config.json')
+
+config.get_metadata()
+config.get_input_image()
+config.get_gps_radius()
+config.get_matching_setup()
